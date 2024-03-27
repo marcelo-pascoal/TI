c48874db87852d757c01bb9cfd1d13a904386b33
marcelo-pascoal <132770891+marcelo-pascoal@users.noreply.github.com>
2024-03-27 16:56:59 +0000

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -19,7 +19,7 @@
   ?>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Dashboard EI-TI</a>
+      <a class="navbar-brand" href="#">Estacionamento</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -48,20 +48,18 @@
   <hr>
   <div class="container">
     <div class="row text-center">
-      <div class="col-sm-4">
+      <div class="col-sm-2">
         <div class="card">
           <div class="card-header sensor">
             <h4>Temperatura 40°</h4>
           </div>
           <div class="card-body">
-            <img src="temperature-high.png">
+            <img src="temperature-high.png" width="20px">
           </div>
           <div class="card-footer">
             <h6><b>Atualização:</b> 2024/03/13 22:09 - <a href="temperatura.html">Histórico</a></h6>
           </div>
         </div>
-      </div>
-      <div class="col-sm-4">
         <div class="card">
           <div class="card-header sensor">
             <h4>Humidade 70°</h4>
@@ -73,8 +71,6 @@
             <h6><b>Atualização:</b> 2024/03/13 22:09 - <a href="humidade.html">Histórico</a></h6>
           </div>
         </div>
-      </div>
-      <div class="col-sm-4">
         <div class="card">
           <div class="card-header atuador">
             <h4>Led Arduino: Ligado</h4>
@@ -87,48 +83,48 @@
           </div>
         </div>
       </div>
+      <div class="col-sm-8">
+        <div class="card">
+          <div class="card-header">
+            <h4>Tabela de Sensores</h4>
+          </div>
+          <div class="card-body">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Tipo de Dispositivo IoT</th>
+                  <th scope="col">Valor</th>
+                  <th scope="col">Data de Avaliação</th>
+                  <th scope="col">Estado Alertas</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Temperatura</td>
+                  <td>40°</td>
+                  <td>2024/03/13 22:09</td>
+                  <td><button type="button" class="btn rounded-pill btn-danger btn-sm">Elevada</button></td>
+                </tr>
+                <tr>
+                  <td>Humidade</td>
+                  <td>70°</td>
+                  <td>2024/03/13 22:09</td>
+                  <td><button type="button" class="btn rounded-pill btn-info btn-sm">Normal</button></td>
+                </tr>
+                <tr>
+                  <td>Led Arduino</td>
+                  <td>Ligado</td>
+                  <td>2024/03/13 22:09</td>
+                  <td><button type="button" class="btn rounded-pill btn-primary btn-sm">Ligado</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <br>
-  <div class="container">
-    <div class="card">
-      <div class="card-header">
-        <h4>Tabela de Sensores</h4>
-      </div>
-      <div class="card-body">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Tipo de Dispositivo IoT</th>
-              <th scope="col">Valor</th>
-              <th scope="col">Data de Avaliação</th>
-              <th scope="col">Estado Alertas</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Temperatura</td>
-              <td>40°</td>
-              <td>2024/03/13 22:09</td>
-              <td><button type="button" class="btn rounded-pill btn-danger btn-sm">Elevada</button></td>
-            </tr>
-            <tr>
-              <td>Humidade</td>
-              <td>70°</td>
-              <td>2024/03/13 22:09</td>
-              <td><button type="button" class="btn rounded-pill btn-info btn-sm">Normal</button></td>
-            </tr>
-            <tr>
-              <td>Led Arduino</td>
-              <td>Ligado</td>
-              <td>2024/03/13 22:09</td>
-              <td><button type="button" class="btn rounded-pill btn-primary btn-sm">Ligado</button></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
