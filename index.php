d92d9f668f53de1f45d29513de9bbf526fcd78f7
marcelo-pascoal <132770891+marcelo-pascoal@users.noreply.github.com>
2024-04-20 18:43:51 +0100

    alteração de metodo de login
    correção usando a matéria das teóricas

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?php
-#verificação de início de sessão 
-#   Verifica se a variável 'username' está definida através de pedido POST.
-#   Caso esteja verifica se ela se encontra no array 'usernames' carregado do ficheiro 'users.txt'
-#   A posição no array irá corresponder à posição no array 'hashes' carregado do ficheiro 
-#'users_hash.txt' que guarda a hash das passwords e a mesma é comparada com o hash obtido 
-#pela 'password' recebida também por POST.
-#   Caso a verificação seja bem sucedida o utilizador é redireccionado para o dashboard e o username
-#é defenido como variável de sessão para posterior verificação. 
+#Login
+#Verifica se as variáveis 'username' e 'password' estão definidas através de pedido POST.
+#       Caso não seja possível abrir o ficheiro de utilizadores é retornado erro de servidor (500)
+#       precorre o ficheiro linha a linha e explode cada uma para validação
+#           Caso o username seja igual ao username fornecido e a password seja validada com o hash guardado
+#               o utilizador é redireccionado para o dashboard
+#               o username é defenido como variável de sessão para posterior verificação. 
 session_start();
-$password_hash = "";
-if (isset($_POST['username'])) {
-    $usernames = array_map('rtrim', file('./users.txt'));
-    $hashes = array_map('rtrim', file('./users_hash.txt'));
-    if ($key = array_search($_POST['username'], $usernames)) {
-        $password_hash = $hashes[$key];
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $users = fopen("./users.txt", "r") or http_response_code(500) && die("Erro ao abrir ficheiro");
+    while (!feof($users)) {
+        $linha = explode(";", fgets($users));
+        if (strcmp($linha[0], $_POST['username']) && password_verify($_POST['password'], $linha[1])) {
+            $_SESSION["username"] = $_POST['username'];
+            fclose($users);
+            header("Location: dashboard.php");
+        }
     }
-}
-if (isset($_POST['password']) and password_verify($_POST['password'], $password_hash)) {
-    $_SESSION["username"] = $_POST['username'];
-    header("Location: dashboard.php");
+    fclose($users);
 }
 ?>
 <!doctype html>
@@ -32,27 +31,22 @@ if (isset($_POST['password']) and password_verify($_POST['password'], $password_
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
+<!--Formulario de login-->
+
 <body>
-
-    <?php
-    /*
-    echo password_hash("password", PASSWORD_DEFAULT);
-    */
-    ?>
-
     <div class="container">
         <div class="row justify-content-center">
             <form class="login" method="post">
                 <a href="index.php"><img src="./img/estg_h.png" alt=""></a>
                 <div class="mb-3">
                     <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="." name="username" required>
+                    <input type="text" class="form-control" id="username" name="username" required>
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="." name="password" required>
+                    <input type="password" class="form-control" id="password" name="password" required>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Login</button>
             </form>
         </div>
     </div>
