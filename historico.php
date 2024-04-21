14be0b4ac781ca6da0eaab5639439a0f5d4c1946
marcelo-pascoal <132770891+marcelo-pascoal@users.noreply.github.com>
2024-04-22 00:29:00 +0100

    fix

--- historico.php
+++ historico.php
@@ -2,26 +2,22 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Plataforma IoT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Plataforma IoT</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <?php
-    session_start();
-    if (!isset($_SESSION['username'])) {
-        header("refresh:5;url=index.php");
-        die("Acesso restrito.");
-    }
-    if (!isset($_GET['nome']) || !is_dir("api/files/" . $_GET['nome'])) {
-        header("refresh:5;url=dashboard.php");
-        die("Historico Inexistente.");
-    }
-    ?>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+    header("refresh:5;url=index.php");
+    die("Acesso restrito.");
+  }
+  ?>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Veículo</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,38 +35,40 @@
       </div>
     </div>
   </nav>
-  <form action="/action_page.php">
-<select name="sensors">
-<option value="temperatura">Temperatura</option>
-<option value="humidade">Humidade</option>
-<option value="iluminacao">Iluminacao</option>
-<option value="portas">Portas</option>
-<option value="ventoinha">Ventoinha</option>
-</select>
-<br><br>
-<input type="submit">
+  <form>
+    <select name="nome">
+      <option value="temperatura">Temperatura</option>
+      <option value="humidade">Humidade</option>
+      <option value="iluminacao">Iluminacao</option>
+      <option value="portas">Portas</option>
+      <option value="ventoinha">Ventoinha</option>
+    </select>
+    <br><br>
+    <input type="submit">
   </form>
 
-    <h1><?php echo $_GET['nome'] ?></h1>
-    <a href="dashboard.php">Voltar Atrás</a>
-    <div>
-        <table class="table">
-            <tr>
-                <th>data</th>
-                <th>valor</th>
-            </tr>
-            <?php
-            $linhasLog = file("api/files/" . $_GET['nome'] . "/log.txt", FILE_IGNORE_NEW_LINES);
-            foreach ($linhasLog as $linha) {
-                echo "<tr><td>";
-                print_r(explode(';', $linha)[0]);
-                echo "</td><td>";
-                print_r(explode(';', $linha)[1]);
-                echo "</td></tr>";
-            }
-            ?>
-        </table>
-    </div>
+  <h1><?php if (isset($_GET['nome'])) echo $_GET['nome'] ?></h1>
+  <a href="dashboard.php">Voltar Atrás</a>
+  <div>
+    <table class="table">
+      <tr>
+        <th>data</th>
+        <th>valor</th>
+      </tr>
+      <?php
+      if (isset($_GET['nome'])) {
+        $linhasLog = file("api/files/" . $_GET['nome'] . "/log.txt", FILE_IGNORE_NEW_LINES);
+        foreach ($linhasLog as $linha) {
+          echo "<tr><td>";
+          print_r(explode(';', $linha)[0]);
+          echo "</td><td>";
+          print_r(explode(';', $linha)[1]);
+          echo "</td></tr>";
+        }
+      }
+      ?>
+    </table>
+  </div>
 </body>
 
 </html>
